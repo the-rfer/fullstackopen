a56e7fdf43b05c06db00b23ef61948773e5dc87f
knc <ruben.fer252@gmail.com>
2024-07-02 01:54:57 +0100

    removed console logging

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,99 +1,98 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import Numbers from "./components/Numbers";
-import ContactForm from "./components/ContactForm";
-import Search from "./components/Search";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Numbers from './components/Numbers';
+import ContactForm from './components/ContactForm';
+import Search from './components/Search';
 
 const App = () => {
-	const [persons, setPersons] = useState([]);
-	const [newName, setNewName] = useState("");
-	const [newNumber, setNewNumber] = useState("");
-	const [search, setSearch] = useState("");
-	const [filteredResults, setFilteredResults] = useState(persons);
+    const [persons, setPersons] = useState([]);
+    const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
+    const [search, setSearch] = useState('');
+    const [filteredResults, setFilteredResults] = useState(persons);
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-		if (newName === "" || newNumber === "") {
-			alert("Please enter a name and a number");
-			return;
-		}
+        if (newName === '' || newNumber === '') {
+            alert('Please enter a name and a number');
+            return;
+        }
 
-		if (persons.some((person) => person.name === newName)) {
-			alert(`${newName} is already in the phonebook`);
-			return;
-		}
+        if (persons.some((person) => person.name === newName)) {
+            alert(`${newName} is already in the phonebook`);
+            return;
+        }
 
-		if (persons.some((person) => person.number === newNumber)) {
-			alert(`${newNumber} is already in the phonebook`);
-			return;
-		}
+        if (persons.some((person) => person.number === newNumber)) {
+            alert(`${newNumber} is already in the phonebook`);
+            return;
+        }
 
-		const newId =
-			persons.length > 0
-				? (Math.max(...persons.map((p) => p.id)) + 1).toString()
-				: "1";
+        const newId =
+            persons.length > 0
+                ? (Math.max(...persons.map((p) => p.id)) + 1).toString()
+                : '1';
 
-		const newPerson = { name: newName, number: newNumber, id: newId };
+        const newPerson = { name: newName, number: newNumber, id: newId };
 
-		axios.post("http://localhost:3001/persons", newPerson).then((response) => {
-			console.log("post response: ", response.data);
-		});
+        axios.post('http://localhost:3001/persons', newPerson);
 
-		setPersons(persons.concat(newPerson));
-		setFilteredResults(persons.concat(newPerson));
-		setNewName("");
-		setNewNumber("");
-	};
+        setPersons(persons.concat(newPerson));
+        setFilteredResults(persons.concat(newPerson));
+        setNewName('');
+        setNewNumber('');
+    };
 
-	const handleSearch = (e) => {
-		let searchValue = e.target.value;
-		setSearch(searchValue);
+    const handleSearch = (e) => {
+        let searchValue = e.target.value;
+        setSearch(searchValue);
 
-		if (searchValue === "") {
-			setFilteredResults(persons);
-			return;
-		}
+        if (searchValue === '') {
+            setFilteredResults(persons);
+            return;
+        }
 
-		setFilteredResults(
-			persons.filter(
-				(person) =>
-					person.name.toLowerCase().includes(searchValue.toLowerCase()) ||
-					person.number.includes(searchValue)
-			)
-		);
-	};
+        setFilteredResults(
+            persons.filter(
+                (person) =>
+                    person.name
+                        .toLowerCase()
+                        .includes(searchValue.toLowerCase()) ||
+                    person.number.includes(searchValue)
+            )
+        );
+    };
 
-	useEffect(() => {
-		axios.get("http://localhost:3001/persons").then((response) => {
-			console.log("response: ", response.data);
-			setPersons(response.data);
-			setFilteredResults(response.data);
-		});
-	}, []);
+    useEffect(() => {
+        axios.get('http://localhost:3001/persons').then((response) => {
+            setPersons(response.data);
+            setFilteredResults(response.data);
+        });
+    }, []);
 
-	return (
-		<div>
-			<h1>Phonebook</h1>
-			<Search
-				search={search}
-				persons={persons}
-				setSearch={setSearch}
-				handleSearch={handleSearch}
-				setFilteredResults={setFilteredResults}
-			/>
+    return (
+        <div>
+            <h1>Phonebook</h1>
+            <Search
+                search={search}
+                persons={persons}
+                setSearch={setSearch}
+                handleSearch={handleSearch}
+                setFilteredResults={setFilteredResults}
+            />
 
-			<ContactForm
-				handleSubmit={handleSubmit}
-				newName={newName}
-				setNewName={setNewName}
-				newNumber={newNumber}
-				setNewNumber={setNewNumber}
-			/>
+            <ContactForm
+                handleSubmit={handleSubmit}
+                newName={newName}
+                setNewName={setNewName}
+                newNumber={newNumber}
+                setNewNumber={setNewNumber}
+            />
 
-			<Numbers filteredResults={filteredResults} />
-		</div>
-	);
+            <Numbers filteredResults={filteredResults} />
+        </div>
+    );
 };
 
 export default App;
